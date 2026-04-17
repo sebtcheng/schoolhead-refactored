0349193f7f78040cb85bf77c049610890f593a14
Clea Monique Sacriz <clea.sacriz@deped.gov.ph>
2026-04-17 15:28:02 +0800

    pgboss

--- src/modules/NexusDashboard.jsx
+++ src/modules/NexusDashboard.jsx
@@ -145,7 +145,7 @@ const NodesDashboard = () => {
             route: '/draft/esf7',
             badge: esf7Status === 'VERIFIED' ? 'VERIFIED' : (esf7Status === 'NOT_STARTED' ? 'BETA' : 'STAGED'),
             description: 'eSF7 will know about teacher and staff loading.',
-            isLocked: false, 
+            isLocked: true, 
         },
         {
             id: 'nspp',
