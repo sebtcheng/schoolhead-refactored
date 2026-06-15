a476ec1ff9190fa1962bd01487c671a3e6284ae3
Clea Monique Sacriz <clea.sacriz@deped.gov.ph>
2026-06-15 13:11:33 +0800

    Clean up duplicates in api/index.js, package.json, and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,6 @@ const handleProxyError = (proxy, _options) => {
 
 export default defineConfig({
   base: '/insighted-schoolhead/',
-  base: '/insighted-schoolhead/',
   define: {
     // Exposes version to the app as import.meta.env.VITE_APP_VERSION
     'import.meta.env.VITE_APP_VERSION': JSON.stringify(version),
@@ -36,7 +35,6 @@ export default defineConfig({
       injectRegister: null,
       manifestFilename: 'manifest.json',
       devOptions: {
-        enabled: false,
         enabled: false,
         type: 'module',
         navigateFallback: 'index.html',
